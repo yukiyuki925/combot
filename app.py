e869d267d3751eea81ec8b1144f0e50e6496dcf5
yukiyuki925 <yuki.work1999@gmail.com>
2024-05-06 16:35:59 +0900

    web driver usr

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 import feedparser
 from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-CHANNEL_ID = "UCpj_nD9850tykDqIrjtIXdg"
 
-# 現状、最新の動画のID
-last_vid = ["bv9ondE2rpg"]
+# 動画検知 -----------------------------------
 
-while True:
-  url = "https://www.youtube.com/feeds/videos.xml?channel_id={}".format(CHANNEL_ID)
-  d = feedparser.parse(url)
-  videoID = d.entries[0].yt_videoid
-  print(videoID,d.entries[0]["title"])
+# CHANNEL_ID = "UCpj_nD9850tykDqIrjtIXdg"
 
-  # 動画IDを比較
-  if videoID not in last_vid:
-    break
-  sleep(5)
+# # 現状、最新の動画のID
+# last_vid = ["bv9ondE2rpg"]
+
+# while True:
+#   url = "https://www.youtube.com/feeds/videos.xml?channel_id={}".format(CHANNEL_ID)
+#   d = feedparser.parse(url)
+#   videoID = d.entries[0].yt_videoid
+#   print(videoID,d.entries[0]["title"])
+
+#   # 動画IDを比較
+#   if videoID not in last_vid:
+#     break
+#   sleep(5)
+
+# pyAutoGUI ------------------------------------------- 
+driver_path="/Users/kumagai/Desktop/combot/tools/chromedriver"
+service = Service(executable_path=driver_path)
+driver = webdriver.Chrome(service=service)
+
+# Googleの検索ページを開く
+url = 'https://www.google.com'
+driver.get(url)
