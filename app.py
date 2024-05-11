692ec0aa3513d9a66f776792f40ee92a6d67a852
yukiyuki925 <yuki.work1999@gmail.com>
2024-05-11 14:48:18 +0900

    pyautogui

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import feedparser
+import pyautogui
+import pyperclip
+import time
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -14,7 +17,7 @@ from selenium.webdriver.chrome.service import Service
 # while True:
 #   url = "https://www.youtube.com/feeds/videos.xml?channel_id={}".format(CHANNEL_ID)
 #   d = feedparser.parse(url)
-#   videoID = d.entries[0].yt_videoid
+  # videoID = d.entries[0].yt_videoid
 #   print(videoID,d.entries[0]["title"])
 
 #   # 動画IDを比較
@@ -28,5 +31,28 @@ service = Service(executable_path=driver_path)
 driver = webdriver.Chrome(service=service)
 
 # Googleの検索ページを開く
+videoID = "https://youtube.com/watch?v=GSqOaaNi2ZM"
 url = 'https://www.google.com'
 driver.get(url)
+
+time.sleep(1)
+
+# GUI
+
+pyautogui.keyDown("command")
+pyautogui.keyDown("l")
+pyautogui.keyUp("command")
+pyautogui.keyUp("l")
+time.sleep(1)
+
+pyperclip.copy(videoID)
+time.sleep(1)
+
+pyautogui.hotkey('command', 'v')
+time.sleep(1)
+
+pyautogui.hotkey('enter')
+time.sleep(1)
+
+pyautogui.moveTo(100,500)
+time.sleep(5)
